7e01b05ef1423f7ed9ebd0971d9b9bee81968875
Alvin Ng <alvinoalvin123@gmail.com>
2019-02-13 04:19:48 -0800

    ModifiedA02

--- A02/Lab2/Lab2.ino
+++ A02/Lab2/Lab2.ino
@@ -1,12 +1,18 @@
 const int pushBtn = 12;
-const int myPins[] = {2, 3, 4, 5, 6, 7};
+const int potent = A0;
+const int photocell = A1;
+const int tempSensor = A2;
+
+const int myPins[] = {2, 3, 4};
 
 long int prevMill = 0;
-int currState = 1;
+int btnState = 0;
+int prevBtnState = 0;
+int state;
 
-char* wind[] = {"Still", "Breezy", "Windy", "Gusty", "Stormy"};
-char* brightness[] = {"Dark", "Overcast", "Bright", "Sunny"};
-char* temp[] = {"Below Freezing", "Cool", "Warm", "Hot"};
+String wind[] = {"Still", "Breezy", "Windy", "Gusty", "Stormy"};
+String brightness[] = {"Dark", "Overcast", "Bright", "Sunny"};
+String temp[] = {"Below Freezing", "Cool", "Warm", "Hot"};
 
 const int tempState = 1;
 const int lightState = 2;
@@ -17,11 +23,13 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(pushBtn, INPUT);
+  pinMode(potent, INPUT);
+  pinMode(tempSensor, INPUT);
+  pinMode(photocell, INPUT);
 
   for (int ledPin : myPins) {
     pinMode(ledPin, OUTPUT);
   }
-
 }
 void loop() {
   unsigned long currentMill = millis();
@@ -30,34 +38,25 @@ void loop() {
   for (int ledPin : myPins) {
     digitalWrite(ledPin, HIGH);
   }
-
- if (pushValue == HIGH) {
-    if (currState < 4) {
-      currState++;
-    }
-    else {
-      currState = 1;
-    }
-    Serial.println(currState);
-  }
-  else
-  pushValue = false;
-
-  if (currentMill - prevMill > 1000) {
+  incrState();  if (currentMill - prevMill > 950) {
     prevMill = currentMill;
-    if (currState = tempState) {
+    if (state == tempState) {
       Serial.println("Temperature");
-      Serial.println("");
+      double c = (int)round(analogRead(tempSensor) * 0.48828125);
+      Serial.print(c);
+      Serial.print(" C ");
+      Serial.print((int)round(1.8*c+32));
+      Serial.println(" F ");
     }
-    else if (currState = lightState) {
+    else if (state == lightState) {
       Serial.println("Light:");
-      Serial.println("");
+      Serial.println(analogRead(photocell));
     }
-    else if (currState = windState) {
-      Serial.println("Light:");
-      Serial.println("");
+    else if (state == windState) {
+      Serial.println("Wind:");
+      Serial.println(analogRead(potent));
     }
-    else if (currState = allState) {
+    else if (state == allState) {
       Serial.println("----------------------------------------------------------- ");
       Serial.println("Count Lake Resort Date: ");
       Serial.println("Location:");
@@ -66,5 +65,19 @@ void loop() {
       Serial.println("Location:");
       Serial.println("Outside Ambient Light: ");;
     }
+  }
+}
+void incrState() {
+  btnState = digitalRead(pushBtn);
+  if (btnState != prevBtnState) {
+    if (btnState == 1) {
+      state++;
+      if (state > 4) {
+        state = 1;
+      }
+//      Serial.println(state);
+    }
+    prevBtnState = btnState;
+    delay(50);
   }
 }
